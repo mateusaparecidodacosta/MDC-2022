99797545fe020c5a7ff904167a314ec496ae3d40
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-05-20 22:13:06 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int dividendo , divisor, resto ;
+
+Console.WriteLine("Máximo divisor Comum (método interativo)\n");
+
+Console.Write("Digite o 1º número (a):");
+dividendo = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Digite o 2º número (a):");
+divisor = Convert.ToInt32(Console.ReadLine()!);
+
+do 
+{
+    resto = dividendo % divisor;
+    dividendo = divisor;
+    divisor = resto;
+}
+ while (resto !=0);
+
+Console.WriteLine($"MDC (a,b) = {dividendo}");
